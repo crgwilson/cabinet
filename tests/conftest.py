447e0ebabeed59e974414b2bb3a0c569268bf7fc
Craig Wilson <craigjwilson93@gmail.com>
2020-11-01 17:22:49 -0500

    Add test for session token with no permissions

--- tests/conftest.py
+++ tests/conftest.py
@@ -108,16 +108,46 @@ def expired_token(session):
     yield token.decode("utf-8")
 
 
+@pytest.yield_fixture
+def tokens(session, user) -> dict:
+    admin_user_id = 1
+    tokens_to_return = {}
+
+    session_factory = SessionFactory()
+    privileged_session = session_factory.new(admin_user_id)
+    expired_session = session_factory.new(admin_user_id, ttl=0)
+    unprivileged_session = session_factory.new(user.id)
+
+    session.commit()
+
+    token_factory = SessionTokenFactory()
+    privileged_token = token_factory.new(privileged_session, test_config.CABINET_SECRET)
+    expired_token = token_factory.new(expired_session, test_config.CABINET_SECRET)
+    unprivileged_token = token_factory.new(
+        unprivileged_session, test_config.CABINET_SECRET
+    )
+
+    tokens_to_return["privileged"] = privileged_token.decode("utf-8")
+    tokens_to_return["expired"] = expired_token.decode("utf-8")
+    tokens_to_return["unprivileged"] = unprivileged_token.decode("utf-8")
+
+    yield tokens_to_return
+
+
 @pytest.yield_fixture(scope="function")
-def headers(token, expired_token) -> dict:
+def headers(tokens) -> dict:
     headers = {
         "valid": {
             "Content-Type": "application/json",
-            "Authorization": f"Bearer {token}",
+            "Authorization": "Bearer " + tokens["privileged"],
         },
         "expired": {
             "Content-Type": "application/json",
-            "Authorization": f"Bearer {expired_token}",
+            "Authorization": "Bearer " + tokens["expired"],
+        },
+        "no_permissions": {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + tokens["unprivileged"],
         },
         "invalid_auth_type": {
             "Content-Type": "application/json",
