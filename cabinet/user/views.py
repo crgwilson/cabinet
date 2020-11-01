41df0f5f55f5a56af7fb298182f2146b1f9d99b1
Craig Wilson <craigjwilson93@gmail.com>
2020-10-31 22:23:09 -0400

    Add permissions to user endpoints

--- cabinet/user/views.py
+++ cabinet/user/views.py
@@ -8,10 +8,14 @@ from flask_restful import Api
 import cabinet.user.controllers as user_controller
 from cabinet._types import ApiObject
 from cabinet.api import CabinetResource
+from cabinet.auth.controllers import permission_required
+from cabinet.response import CabinetApiResponse
 from cabinet.schema import validate_schema
 from cabinet.user.schemas import user_input_schema, user_list_schema, user_schema
 
 # from cabinet.response import CabinetApiResponse
+
+API_OBJECT = "User"
 
 logger = get_logger(__name__)
 
@@ -20,12 +24,15 @@ api = Api(bp)
 
 
 class UsersList(CabinetResource):
+    # TODO: The gets shouldn't say they return Any
+    @permission_required(API_OBJECT)
     def get(self) -> Any:
         result = user_controller.get_all_users()
         rendered_result = self.render(result, user_list_schema)
 
         return rendered_result
 
+    @permission_required(API_OBJECT)
     @validate_schema(user_input_schema)
     def post(self, data: ApiObject) -> Any:
         result = user_controller.create_user(
@@ -36,4 +43,41 @@ class UsersList(CabinetResource):
         return rendered_result
 
 
+class Users(CabinetResource):
+    @permission_required(API_OBJECT)
+    def get(self, user_id: int) -> Any:
+        result = user_controller.get_user(user_id)
+        if not result:
+            rendered_result = CabinetApiResponse.not_found()
+        else:
+            rendered_result = self.render(result, user_schema)
+
+        return rendered_result
+
+    @permission_required(API_OBJECT)
+    @validate_schema(user_input_schema)
+    def put(self, user_id: int, data: ApiObject) -> Any:
+        target_user = user_controller.get_user(user_id)
+        if not target_user:
+            return CabinetApiResponse.not_found()
+
+        result = user_controller.update_user(
+            user_id, username=data["username"], password=data["password"]
+        )
+
+        rendered_result = self.render(result, user_schema)
+
+        return rendered_result
+
+    @permission_required(API_OBJECT)
+    def delete(self, user_id: int) -> Any:
+        target_user = user_controller.get_user(user_id)
+        if not target_user:
+            return CabinetApiResponse.not_found()
+
+        user_controller.delete_user(user_id)
+        return CabinetApiResponse.ok()
+
+
 api.add_resource(UsersList, "/users", endpoint="users")
+api.add_resource(Users, "/users/<int:user_id>", endpoint="user")
--- tests/test_user.py
+++ tests/test_user.py
@@ -1,8 +1,18 @@
 import json
 
 
-def test_users_get(client, helpers) -> None:
-    response = client.get("/api/v1/users")
+def test_dont_store_user_passwords(user_predictable_password) -> None:
+    assert user_predictable_password.password != "password"
+    assert user_predictable_password.check_password("password")
+
+
+# TODO: paramaterize some of these tests
+def test_users_get(client, session_token, helpers) -> None:
+    headers = helpers.headers(session_token)
+    response = client.get(
+        "/api/v1/users",
+        headers=headers,
+    )
 
     helpers.assert_success(response)
 
@@ -11,14 +21,44 @@ def test_users_get(client, helpers) -> None:
 
     first_element = response.json[0]
     assert first_element["username"] == "admin"
+    assert isinstance(first_element["roles"], list)
     assert "password" not in first_element
 
 
-def test_users_post(client, helpers) -> None:
+def test_users_post(client, session_token, helpers) -> None:
+    headers = helpers.headers(session_token)
     response = client.post(
         "/api/v1/users",
         data=json.dumps({"username": "testuser1", "password": "reallygoodpassword"}),
-        content_type=helpers.CONTENT_TYPE,
+        headers=headers,
     )
+
+    helpers.assert_success(response)
+
+
+def test_user_get(client, session_token, helpers, user) -> None:
+    headers = helpers.headers(session_token)
+    response = client.get(
+        f"/api/v1/users/{user.id}",
+        headers=headers,
+    )
+
+    helpers.assert_success(response)
+
+
+def test_user_put(client, session_token, helpers, user) -> None:
+    headers = helpers.headers(session_token)
+    response = client.put(
+        f"/api/v1/users/{user.id}",
+        headers=headers,
+        data=json.dumps({"username": user.username, "password": "changedpassword"}),
+    )
+
+    helpers.assert_success(response)
+
+
+def test_user_delete(client, session_token, helpers, user) -> None:
+    headers = helpers.headers(session_token)
+    response = client.delete(f"/api/v1/users/{user.id}", headers=headers)
 
     helpers.assert_success(response)
